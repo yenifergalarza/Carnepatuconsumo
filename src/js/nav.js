063ee1c58a60cf1dadbe0b722754ca6821cfc6c8
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-11-26 15:35:19 -0500

    cambios de html

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,6 @@
-const menuclick = document.getElementById("men1");
-const liToggle = document.getElementById("menu");
+// const menuclick = document.getElementById("men1");
+// const liToggle = document.getElementById("menu");
 
-menuclick.addEventListener("click", () => {
-    liToggle.classList.toggle("menu-none");
-});
+// menuclick.addEventListener("click", () => {
+//     liToggle.classList.toggle("menu-none");
+// });
