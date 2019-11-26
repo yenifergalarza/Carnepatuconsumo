78e07a4815d5be468daa67e425ba1a2c369c898e
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-11-26 14:56:44 -0500

    cambios

---
+++ src/js/nav.js
@@ -0,0 +1,6 @@
+const menuclick = document.getElementById("men1");
+const liToggle = document.getElementById("menu");
+
+menuclick.addEventListener("click", () => {
+    liToggle.classList.toggle("menu-none");
+});
