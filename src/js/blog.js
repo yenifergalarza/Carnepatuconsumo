2e2015178bceb9704719e66bebf91eb7875d4ef5
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-12-03 16:45:55 -0500

    blog ok vista desktop,sin embargo falta hacer para cada sub blog (repetir la estructura)

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,1 +1,21 @@
-//div.style.transform = "translateX(someValue)"
+const arrowBlog1 = document.getElementById("arrow1B");
+const allBlogs = document.getElementById("allBlogs");
+const blog1 = document.getElementById("blog1");
+
+const funcOfMeat1 = () => {
+    allBlogs.classList.remove("dBlock");
+    blog1.classList.remove("dNone");
+    allBlogs.classList.add("dNone");
+
+}
+
+
+arrowBlog1.addEventListener("click", () => {
+    const pagina = "./blog0.html"
+
+    function redireccionar() {
+        location.href = pagina
+    }
+    setTimeout(redireccionar, 100);
+
+});
