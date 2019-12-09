e12858e22f71827b489f1e47e3c1daa925f3ce6a
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-12-09 16:00:05 -0500

    intento de captcha

--- src/php/enviar.php
+++ src/php/enviar.php
@@ -1,37 +1,60 @@
-<?php 
-//llamando a ls campos 
-//$nombre = $_POST['nombre'];
-//$apellido = $_POST['apellido'];
-//$direccion = $_POST['direccion'];
-//$correo = $_POST['correo'];
-//$telefono = $_POST['telefono'];
-//$mensaje = $_POST['mensaje'];
 
-//datos consultas@diamanteperu.com
-//$destinatario="yeniferalexandragapezo@gmail.com";
-//$asunto = "contacto desde nuestra web";
-
-//$carta = "De : $nombre + $apellido \n ";
-//$carta .= "Desde : $direccion \n ";
-//$carta .= "Correo : $correo \n ";
-//$carta .= "Telefono : $telefono \n "
-//$carta .= "Mensaje : $mensaje  ";
-
-
-//enviando Mensaje
-//mail($destinatario,$asunto,$carta);
-//header('Location:Home.jsx');
-
-?> 
+                                                       
 
 
 <?php
-$destino =$_POST["correo"];
-$nombre = $_POST;
-$correo="yenifer@henribarret.com";
-$telefono = $_POST["telefono"];
-$mensaje = $_POST["mensaje"];
-$contenido="Nombre: ".$nombre + $apellido."\nCorreo: ".$correo."\ntelefono:".$telefono."\nMensaje: ".$mensaje."\nDireccion: ".$direccion;
- mail($destino,"Contacto", $contenido);
- header("Location: contacto.html");
-                                                            ?>
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$direccion = $_POST['direccion'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$body = "De : $nombre  $apellido <br> ";
+$body .= "Dirección : $direccion <br> ";
+$body .= "Correo : $correo <br> ";
+$body .= "Telefono : $telefono <br> ";
+$body .= "Mensaje : $mensaje <br> ";
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './Exception.php';
+require './PHPMailer.php';
+require './SMTP.php';
+
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer;
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 3;                    // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'yeniferalexandragapezo@gmail.com';                     // SMTP username
+    $mail->Password   = 'Brazilazulbb123';                               // SMTP password
+    $mail->SMTPSecure = "tls";         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted orrrr ssl
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('yeniferalexandragapezo@gmail.com', 'yenifer');
+    $mail->addAddress('yeniferalexandragapezo@gmail.com', 'Joe User');     // Add a recipient
+   
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Hola envio el correo desde local host';
+    $mail->Body    = $body;
+    $mail -> CharSet = 'UTF-8';
+    $mail->send();
+    echo '<script>
+    alert("el mensaje se envio correctamente");
+    window.history.go(-1);
+    </script>
+    ';
+} catch (Exception $e) {
+    echo "hubo un error al enviar: {$mail->ErrorInfo}";
+} ?>
