2c6bbe207fbe76828a445e3dbc76d1f79edee091
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-12-10 17:33:51 -0500

    cambios

--- src/js/blog.js
+++ src/js/blog.js
@@ -1,7 +1,6 @@
 const arrowBlog1 = document.getElementById("arrow1B");
 const allBlogs = document.getElementById("allBlogs");
 const blog1 = document.getElementById("blog1");
-
 const funcOfMeat1 = () => {
     allBlogs.classList.remove("dBlock");
     blog1.classList.remove("dNone");
